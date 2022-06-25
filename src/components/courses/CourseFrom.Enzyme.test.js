185ea402e67904501d28d97c9a77444192e2b1fa
Mihaela <mihaela.chervenkova@gmail.com>
2022-06-25 20:01:39 +0300

    initial commit

---
+++ src/components/courses/CourseFrom.Enzyme.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CourseForm from "./CourseForm";
+import { shallow } from "enzyme";
+
+function renderCourseForm(args) {
+  const defaultProps = {
+    authors: [],
+    course: {},
+    saving: false,
+    errors: {},
+    onSave: () => {},
+    onChange: () => {},
+  };
+
+  const props = { ...defaultProps, ...args };
+  return shallow(<CourseForm {...props} />);
+}
+
+it("renders form and header", () => {
+  const wrapper = renderCourseForm();
+  expect(wrapper.find("form")).length.toBe(1);
+});
